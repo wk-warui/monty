25f3e62f3069edf68ddd28c4fe506a2b6523ad39
wk-warui <demarazeandrena@gmail.com>
2022-12-15 09:25:56 -0800

    adds an element at the top of a stack

--- push.c
+++ push.c
@@ -1,12 +1,11 @@
 #include "monty.h"
 
 /**
- * check_for_digit - checks that a string only containes digits
+ * check_for_digit - checks that a string only contains digits
  * @str: string to check
  *
- * Return: 0 if only digits, and 1 if otherwise.
+ * Return: 0 if only digits, and 1 if otherwise
  */
-
 static int check_for_digit(char *str)
 {
 	int n;
@@ -22,13 +21,12 @@ static int check_for_digit(char *str)
 }
 
 /**
- * m_push - Adds an element at the top o stack
+ * m_push - push an integer onto the stack
  * @stack: double pointer to the beginning of the stack
  * @line_number: script line number
  *
  * Return: void
  */
-
 void m_push(stack_t **stack, unsigned int line_number)
 {
 	char *str;
@@ -37,15 +35,15 @@ void m_push(stack_t **stack, unsigned int line_number)
 	str = strtok(NULL, "\n\t\r ");
 	if (str == NULL || check_for_digit(str))
 	{
-		dprint(STDOUT_FILENO,
-				"L%u: usage: push integer\n",
-				line_number);
+		dprintf(STDOUT_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
 	num = atoi(str);
 	if (!add_node(stack, num))
 	{
-		dprint(STDOUT_FILENO, "Error: malloc failed\n");
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	var.stack_len++;
